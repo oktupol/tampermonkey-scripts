c8e3bd6ce9ef2610d0ca31e8f7feb079916c737c
Sebastian Wieland <git-commits@nwie.land>
2026-02-20 23:30:48 +0100

    Bump version

--- zeit-komplettansicht.js
+++ zeit-komplettansicht.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zeit.de Komplettansicht
-// @namespace    https://sebastianwie.land/
-// @version      2026-02-20_1
+// @namespace    https://github.com/oktupol/tampermonkey-scripts/
+// @version      2026-02-21
 // @description  Mehrseitige Artikel auf Zeit.de werden immer in der Komplettansicht geöffnet, sodass man innerhalb des Artikels nicht mehr blättern muss.
 // @author       Sebastian Wieland
 // @match        https://www.zeit.de/*
