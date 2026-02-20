491f3d0a5414dbec95220b14206ec37c770eea59
Sebastian Wieland <git-commits@nwie.land>
2026-02-20 22:55:41 +0100

    Update info

--- zeit-komplettansicht.js
+++ zeit-komplettansicht.js
@@ -7,6 +7,9 @@
 // @match        https://www.zeit.de/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zeit.de
 // @grant        none
+// @version      1
+// @downloadURL  https://raw.githubusercontent.com/oktupol/tampermonkey-scripts/refs/heads/main/zeit-komplettansicht.js
+// @updateURL    https://raw.githubusercontent.com/oktupol/tampermonkey-scripts/refs/heads/main/zeit-komplettansicht.js
 // ==/UserScript==
 
 (function() {
